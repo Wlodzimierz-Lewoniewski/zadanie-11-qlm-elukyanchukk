bdad5c1c1ccd96170cc98066652ba6b6268bba83
Elena Lukyanchuk <e.lukyanchukk@gmail.com>
2025-01-13 21:03:51 +0100

    final version

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+import string
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
+n_docs = int(input())
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+docs_tokens = []
+
+for n in range(n_docs): 
+    doc = input().strip().lower()
+    doc = doc.translate(str.maketrans('', '', string.punctuation))
+    doc_tokens = doc.split()
+    docs_tokens.append(doc_tokens)
+
+q = input()
+q_tokens = q.split()
+
+lambd = 0.5
+
+L_d = [len(doc_tokens) for doc_tokens in docs_tokens]
+
+c = [token for doc_tokens in docs_tokens for token in doc_tokens]
+L_c = len(c)
+
+token_counts = [[doc.count(token) for token in q_tokens] for doc in docs_tokens]
+P_t_Md = [[count / len for count in sublist] for sublist, len in zip(token_counts, L_d)]
+
+token_counts_c = [c.count(token) for token in q_tokens]
+P_t_Mc = [count/L_c for count in token_counts_c]
+
+P_t_d = [[lambd * count + (1-lambd) * M_j for count, M_j in zip(counts, P_t_Mc)] for counts in P_t_Md]
+
+P_t_d_multiplied = [eval('*'.join(map(str, P_t_dj))) for P_t_dj in P_t_d]
+
+result = sorted(range(len(P_t_d_multiplied)), key=lambda i: P_t_d_multiplied[i], reverse=True)
+
+print(result)
